ccba678b33c91788554783ab152b240ac598bf05
Sourish3605 <sourishnarendrula@gmail.com>
2025-10-30 15:51:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  // other settings
+  preview: {
+    allowedHosts: ['voyageai-xtoq.onrender.com'],
+    port: 10000,
   },
   plugins: [react()],
   resolve: {
